b9e8e259a0ef58fcd9b6340e7954f7f1be1e53c0
yamada <your.email@example.com>
2019-06-25 17:07:02 +0900

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
  root 'static_pages#home'
-  get 'static_pages/home'
+  get  '/help',    to: 'static_pages#help'
+  get  '/about',   to: 'static_pages#about'
+  get  '/signup',  to: 'users#new'
+  
+  resources :users
 
-  get 'static_pages/help'
-
-  get  'static_pages/about'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'application#hello'
